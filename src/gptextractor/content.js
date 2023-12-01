fa6a99c0028f29269c9db1c1a6810a100d2eaa00
Mohammad Reza Taesiri <mtaesiri@gmail.com>
2023-11-30 23:37:10 -0700

    Added Images to the JSON as Base64

--- src/gptextractor/content.js
+++ src/gptextractor/content.js
@@ -1,11 +1,33 @@
-const parseDOMToJSON = () => {
-    // Extract images
-    const images = Array.from(document.querySelectorAll("img")).map(img => ({
-        alt: img.alt,
-        src: img.src,
-        width: img.width,
-        height: img.height
-    }));
+const parseDOMToJSON = async () => {
+
+    // Helper function to convert image to Base64
+    const toBase64 = async (url) => {
+        try {
+            const response = await fetch(url);
+            const blob = await response.blob();
+            return new Promise((resolve, reject) => {
+                const reader = new FileReader();
+                reader.onloadend = () => resolve(reader.result);
+                reader.onerror = reject;
+                reader.readAsDataURL(blob);
+            });
+        } catch (error) {
+            console.error('Error converting image to Base64:', error);
+            return null; // Return null or some default value
+        }
+    };
+
+
+    // Extract images with Base64 conversion
+    const images = await Promise.all(
+        Array.from(document.querySelectorAll("img")).map(async (img) => ({
+            alt: img.alt,
+            src: await toBase64(img.src),
+            width: img.width,
+            height: img.height
+        }))
+    );
+
 
     // Function to extract message content
     const extractMessageContent = (message) => {
@@ -19,6 +41,7 @@ const parseDOMToJSON = () => {
 
         return { textContent, messageImages };
     };
+
 
     // Extract messages
     const messages = Array.from(document.querySelectorAll("div[data-message-id]")).map(message => ({
@@ -41,25 +64,30 @@ const downloadJSON = (jsonData) => {
     chrome.runtime.sendMessage({ message: "download_json_data", data: jsonData });
 };
 
+
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    // Check if the incoming request is to parse DOM or trigger download
     if (request.message === "parse_dom" || request.message === "trigger_download") {
-        const result = parseDOMToJSON();
-        sendResponse(result);
+        parseDOMToJSON().then(result => {
+            sendResponse(result);
 
-        if (request.message === "trigger_download") {
-            // Trigger download
-            const resultStr = JSON.stringify(result, null, 2);
-            const blob = new Blob([resultStr], { type: "application/json" });
-            const url = URL.createObjectURL(blob);
+            if (request.message === "trigger_download") {
+                // If the message is to trigger download, then process the result for download
+                const resultStr = JSON.stringify(result, null, 2);
+                const blob = new Blob([resultStr], { type: "application/json" });
+                const url = URL.createObjectURL(blob);
 
-            // Creating a temporary link to trigger download
-            const downloadLink = document.createElement('a');
-            downloadLink.href = url;
-            downloadLink.download = "extracted_data.json";
-            document.body.appendChild(downloadLink);
-            downloadLink.click();
-            document.body.removeChild(downloadLink);
-            URL.revokeObjectURL(url);
-        }
+                // Create a temporary link to trigger download
+                const downloadLink = document.createElement('a');
+                downloadLink.href = url;
+                downloadLink.download = "extracted_data.json";
+                document.body.appendChild(downloadLink);
+                downloadLink.click();
+                document.body.removeChild(downloadLink);
+                URL.revokeObjectURL(url);
+            }
+        });
+
+        return true; // Indicates an asynchronous response will be sent
     }
 });
